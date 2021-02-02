07ca6e4c7bfaa8e67ec58d8c0b861d5128158bd1
Przepioreczka <beataa.kedzierska@gmail.com>
2021-02-02 12:29:44 +0100

    Add argparse

--- plot_graphs.py
+++ plot_graphs.py
@@ -2,6 +2,16 @@ from get_graphs import get_data_params, plot_graphs, get_params
 import pandas as pd
 import sys
 from os import listdir
+import argparse
+parser = argparse.ArgumentParser(add_help=True, description = 'You can pass directory to specific xls file or to '
+                                                              'directory where xls data is stored. '
+                                                              'If not provided in file, '
+                                                              'program will ask for filename for png files. ')
+
+parser.add_argument('directory', type=str, help='Directory where xls data is stored')
+parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+
+args = parser.parse_args()
 
 sys.tracebacklimit = 0
 
@@ -17,7 +27,7 @@ def plot_save(directory: str):
         plot_graphs(x, y, colors, title, sub_title, file_name, width=1200, height=height)
 
 
-direc = input('Enter path to your data:')
+direc = args.directory
 if '.xls' == direc[-4:]:
     plot_save(direc)
 
